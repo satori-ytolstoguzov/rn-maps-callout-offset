09e3e25e74c525e3e154f690aa2bb5450ead2c13
Yuriy Tolstoguzov <ytolstoguzov@mz.com>
2018-01-05 12:27:52 -0800

    Added example project to reproduce animation error on android

--- App.js
+++ App.js
@@ -13,33 +13,54 @@ import {
 import MapView from 'react-native-maps';
 
 export default class App extends Component<{}> {
+
+    constructor(props) {
+        super(props);
+
+        this.initialRegion = {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        };
+        this.state = {
+            markers: [
+                { id: 1, coordinate: { latitude: 37.796, longitude: -122.434 }, color: 'green' },
+                { id: 2, coordinate: { latitude: 37.804, longitude: -122.427 }, color: 'blue' },
+                { id: 3, coordinate: { latitude: 37.8, longitude: -122.43 }, color: 'red' }
+            ]
+        };
+
+        setInterval(() => {
+            this.state.markers.forEach((marker) => {
+                // Randomly setting callout every 0.2 sec
+                marker.callout = Math.floor(Math.random() * 10) % 2 === 1;
+            });
+            this.setState({ markers: this.state.markers });
+        }, 200);
+    }
+
+    getMarkers() {
+        return this.state.markers.map((markerData) => (
+            <MapView.Marker
+                coordinate={markerData.coordinate}
+                identifier={markerData.id.toString()}
+                key={markerData.id}
+            >
+                <View style={[styles.marker, {backgroundColor: markerData.color}]} />
+                {markerData.callout ? <MapView.Callout tooltip><Text>Abc</Text> </MapView.Callout> : null }
+            </MapView.Marker>));
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Callout</Text>
+                <Text>Removing a view with index above child</Text>
                 <MapView style={styles.map}
-                         initialRegion={{
-                             latitude: 37.78825,
-                             longitude: -122.4324,
-                             latitudeDelta: 0.0922,
-                             longitudeDelta: 0.0421,
-                         }}
+                         initialRegion={this.initialRegion}
                          provider={MapView.PROVIDER_GOOGLE}
                 >
-                    <MapView.Marker coordinate={{
-                        latitude: 37.8,
-                        longitude: -122.43
-                    }}
-                                    calloutOffset={{x: 100, y: 0}}
-                    >
-                        <View style={styles.marker} />
-                        <MapView.Callout tooltip>
-                            <View style={styles.calloutContainer}>
-                                <Text style={styles.calloutText1}>1st Callout</Text>
-                                <Text style={styles.calloutText2}>2nd Callout</Text>
-                            </View>
-                        </MapView.Callout>
-                    </MapView.Marker>
+                    {this.getMarkers()}
                 </MapView>
             </View>
         );
@@ -63,14 +84,5 @@ const styles = StyleSheet.create({
         width: 30,
         height: 30,
     },
-    calloutText1: {
-        backgroundColor: 'blue',
-    },
-    calloutText2: {
-        backgroundColor: 'green',
-    },
-    calloutContainer: {
-        flexDirection: 'row',
-    }
 });
 
