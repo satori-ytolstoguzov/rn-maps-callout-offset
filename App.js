8e5abf94144766e53bce40c67441e0a5d08c720d
Yuriy Tolstoguzov <ytolstoguzov@mz.com>
2018-01-11 11:47:10 -0800

    Added example of wrong text layout in callout

--- App.js
+++ App.js
@@ -26,18 +26,8 @@ export default class App extends Component<{}> {
         this.state = {
             markers: [
                 { id: 1, coordinate: { latitude: 37.796, longitude: -122.434 }, color: 'green' },
-                { id: 2, coordinate: { latitude: 37.804, longitude: -122.427 }, color: 'blue' },
-                { id: 3, coordinate: { latitude: 37.8, longitude: -122.43 }, color: 'red' }
             ]
         };
-
-        setInterval(() => {
-            this.state.markers.forEach((marker) => {
-                // Randomly setting callout every 0.2 sec
-                marker.callout = Math.floor(Math.random() * 10) % 2 === 1;
-            });
-            this.setState({ markers: this.state.markers });
-        }, 200);
     }
 
     getMarkers() {
@@ -48,14 +38,14 @@ export default class App extends Component<{}> {
                 key={markerData.id}
             >
                 <View style={[styles.marker, {backgroundColor: markerData.color}]} />
-                {markerData.callout ? <MapView.Callout tooltip><Text>Abc</Text> </MapView.Callout> : null }
+                <MapView.Callout tooltip><Text>This text should be one-liner</Text></MapView.Callout>
             </MapView.Marker>));
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Removing a view with index above child</Text>
+                <Text>Text in callout</Text>
                 <MapView style={styles.map}
                          initialRegion={this.initialRegion}
                          provider={MapView.PROVIDER_GOOGLE}
